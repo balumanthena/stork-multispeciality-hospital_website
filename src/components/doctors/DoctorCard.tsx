71cfacee5dc6e5f6de328c3789565b8e1e8c1f57
balumanthena <balumanthena18@gmail.com>
2026-04-20 16:21:26 +0530

    fix: Correct JSX nesting in DoctorCard

--- src/components/doctors/DoctorCard.tsx
+++ src/components/doctors/DoctorCard.tsx
@@ -38,7 +38,6 @@ export function DoctorCard({
               className="object-cover group-hover:scale-110 transition-transform duration-500"
             />
           </div>
-          </div>
         </div>
 
         {/* RIGHT: Content */}
